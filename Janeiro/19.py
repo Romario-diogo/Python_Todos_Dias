dfb3475359885fe5d6b3af626118e459cdd64316
2506roma <romariodiogo2021@gmail.com>
2026-01-20 17:54:53 -0300

    exercise two completed

--- Janeiro/19.py
+++ Janeiro/19.py
@@ -5,7 +5,11 @@ while True:
         break
     else:
         print("Numero incorreto, tetente novamente! ")
-
+cont1 = 0
 cont = 0
 for i in range(0, numero + 1 , 2):
+    cont += i
+    cont1 += 1
     print(i)
+print(f"Soma dos pares: {cont}")
+print(f"Total de pares: {cont1}")
