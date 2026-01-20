095451475bae5b4cd489832ba8dbd2a7555495e5
2506roma <romariodiogo2021@gmail.com>
2026-01-20 20:23:28 -0300

    starting exercise trhee.0

---
+++ Janeiro/20/20.1.py
@@ -0,0 +1,7 @@
+while True:
+    escolha = input("1 - Digite intervalo (início e final)").strip()
+    escolha = input("2 - Mostre resumno do último intervalo").strip()
+    escolha = input("3 - Sair").strip()
+    
+    if escolha.isdigit():
+        
