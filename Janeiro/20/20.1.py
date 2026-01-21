552dff16362f099b793c189d1fc1fb793029849e
2506roma <romariodiogo2021@gmail.com>
2026-01-20 21:39:01 -0300

    Exercise three or four rsrsr completed

--- Janeiro/20/20.1.py
+++ Janeiro/20/20.1.py
@@ -1,7 +1,69 @@
-while True:
-    escolha = input("1 - Digite intervalo (início e final)").strip()
-    escolha = input("2 - Mostre resumno do último intervalo").strip()
-    escolha = input("3 - Sair").strip()
-    
-    if escolha.isdigit():
+
+
+def inicio_final():
+        while True:
+            inicio = input("Digite um numero inteiro ! ").strip()
+            if inicio.isdigit():
+                global inicio_int
+                inicio_int = int(inicio)
+                break
+            else:
+                print("Digite um numero inteiro !")
+        while True:
+            final = input("Digite um numero final !").strip()
+            if final.isdigit():
+                global final_int
+                final_int = int(final)
+                if final_int > inicio_int:
+                    break
+                else:
+                    print("Numero final é menor ou igual ao do inicio")
+
+        return inicio, final 
+
+def mostrar():
+    par = 0 
+    impar = 0
+    soma_impar = 0
+    soma_par = 0
+
+    for i in range(inicio_int, final_int + 1):
         
+        if i % 2 == 0:
+            soma_par += i
+            par += 1
+        
+            print(f"{i} é par")
+        else:
+            soma_impar += i
+            impar += 1
+            print(f"{i} é ímpar")
+        
+    print(f"Total de pares: {par}")
+    print(f"Total de ímpares: {impar}")
+    print(f"Soma dos pares: {soma_par}")
+    print(f"Soma dos ímpares: {soma_impar}")
+
+tem_intervalo = False
+
+while True:
+    print("1 - Digite intervalo (início e final)")
+    print("2 - Mostre resumno do último intervalo")
+    print("3 - Sair")
+    escolha = input(" ")
+    if escolha.isdigit():
+        entrada = int(escolha)  
+        if entrada == 1:
+            inicio_final()
+            tem_intervalo = True
+        elif entrada == 2:
+            if not tem_intervalo:
+                print("Nenhum intervalo cadastrado ainda.")
+            else:
+                mostrar()
+            
+        elif entrada == 3:
+            break
+        else:
+            print("Escolha uma das opções")
+
