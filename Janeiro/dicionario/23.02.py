2605bdc70cc628f22a163830f06d5e480588c338
2506roma <romariodiogo2021@gmail.com>
2026-01-23 20:09:42 -0300

    pause

--- Janeiro/dicionario/23.02.py
+++ Janeiro/dicionario/23.02.py
@@ -17,13 +17,43 @@ cupom_desconto = {
     "NOVCLIENTE": 30
 }
 
+def cadastro_cliente():
+# Validação Nome
+    while True:
+        nome = input("Digite o nome do Cliente: ")
+        if nome.isalpha():
+            cadastro["Nome"]=nome
+            break
+        else:
+            print("Nome digitado para cadastro incorreto, tente novamente!")
+# Validação Idade 
+    while True:
+        idade = input("Digite a idade do cliente: ").strip()
+        if idade.isdigit():
+            cadastro["Idade"]=int(idade)
+            break
+        else:
+            print("Idade digitada incorreta, tente novamente")
+    return
+
+
 while True:
     print("1 - Cadastro")
-    print("2 Lista de mercadoria")
-    print("3 Lista de cupom de desconto")
+    print("2 - Ver cadastro")
+    print("3 - Lista de mercadoria")
+    print("4 - Adicionar mercadoria")
+    print("5 - Finalizar compra")
     escolha = input("").strip()
-    if escolha.isdigit() and int(escolha) > 0 and int(escolha) < 4:
-        print(escolha)
-        break
+    if escolha.isdigit() and int(escolha) > 0 and int(escolha) < 6:
+
+        match int(escolha):
+            case 1:
+                cadastro_cliente()
+            case 2:
+                print(cadastro)
+            case 3:
+                for i in mercadoria:
+                    print(i)
+
     else:
         print("Escolha uma das opções !")
