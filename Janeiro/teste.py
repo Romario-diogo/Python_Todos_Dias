1fcfa59b6f726ecc929cb1d034d0a384a74a2405
2506roma <romariodiogo2021@gmail.com>
2026-01-30 20:45:10 -0300

    push

--- Janeiro/teste.py
+++ Janeiro/teste.py
@@ -4,6 +4,7 @@ idade = int(idade)
 if idade < 18:
     print("=============================================================")
     print(f"Olá,{nome} voce não pode digirir porque tem {idade} anos !!!")
+    
 else:
     print("================================================")
     print(f"Olá {nome} voce pode dirigir por que tem {idade} anos")
