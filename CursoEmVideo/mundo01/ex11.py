30ff7f454d49ad84cb16b54f356eaf8c2d8a8f84
2506roma <romariodiogo2021@gmail.com>
2026-02-03 19:42:44 -0300

    calculo de metros quadrado

---
+++ CursoEmVideo/mundo01/ex11.py
@@ -0,0 +1,16 @@
+while True:
+    try:
+        largura = float(input("Digite a largura da parede: "))
+        altura = float(input("Digite a altura da parede: "))
+        if largura < 0:
+            print("A Largura não pode ser negativo.")
+            continue
+        if altura < 0:
+            print("A Altura não pode ser negativo.")
+            continue
+        break
+    except ValueError:
+        print("valor incorreto!")
+
+print(f"Largura {largura} * Altura {altura} = metros quadrados {largura*altura:.2f}")
+print(f"Vamos precisar de {(largura*altura) / 2:.2f} litros de tinta!")
